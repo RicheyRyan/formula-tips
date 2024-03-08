b1b058a222b209983a45683993f19ee5129c5905
Richey Ryan <richeyryan@proton.me>
2024-03-08 09:09:59 +0000

    Fix: build

--- src/features/timings/components/TimingsTable.tsx
+++ src/features/timings/components/TimingsTable.tsx
@@ -2,35 +2,34 @@ import { Accessor, Component, For } from "solid-js";
 
 const columns = ["Driver", "Start position", "End position", "Best lap time"];
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const DriverRow: Component<{
-  driver: string;
-  driverImage: string;
-  startPosition: number;
-  endPosition: number;
-  bestLapTime: number;
-}> = (props) => {
-  return (
-    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-      <th
-        scope="row"
-        class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
-      >
-        <img
-          class="w-10 h-10 rounded-full"
-          src={props.driverImage}
-          alt={props.driver}
-        />
-        <div class="ps-3">
-          <div class="text-base font-semibold">{props.driver}</div>
-        </div>
-      </th>
-      <td class="px-6 py-4">{props.startPosition}</td>
-      <td class="px-6 py-4">{props.endPosition}</td>
-      <td class="px-6 py-4">{props.bestLapTime}</td>
-    </tr>
-  );
-};
+// const DriverRow: Component<{
+//   driver: string;
+//   driverImage: string;
+//   startPosition: number;
+//   endPosition: number;
+//   bestLapTime: number;
+// }> = (props) => {
+//   return (
+//     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+//       <th
+//         scope="row"
+//         class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+//       >
+//         <img
+//           class="w-10 h-10 rounded-full"
+//           src={props.driverImage}
+//           alt={props.driver}
+//         />
+//         <div class="ps-3">
+//           <div class="text-base font-semibold">{props.driver}</div>
+//         </div>
+//       </th>
+//       <td class="px-6 py-4">{props.startPosition}</td>
+//       <td class="px-6 py-4">{props.endPosition}</td>
+//       <td class="px-6 py-4">{props.bestLapTime}</td>
+//     </tr>
+//   );
+// };
 
 export const TimingsTable: Component<{
   sessionKey: Accessor<number>;
